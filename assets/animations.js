971ef7da38288e47935f3a9ee973616ba044949e
Serena Wang <serenawa@andrew.cmu.edu>
2020-12-12 01:48:19 -0800

    fruit tart page finished

--- assets/animations.js
+++ assets/animations.js
@@ -1,2 +1,26 @@
 // for clicking buttons/animatiosn for HW 8
 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+//next and prev
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	if (n>slides.length) {slideIndex = 1}
+	if (n<1) {slideIndex = slides.length}
+	for (i=0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slides[slideIndex-1].style.display="block";
+}// for clicking buttons/animatiosn for HW 8
+
+console.log(slideIndex)
